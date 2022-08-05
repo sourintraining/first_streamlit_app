8213e4e3b4f34ece18890cfa0ac814813479c795
sourintraining <108939079+sourintraining@users.noreply.github.com>
2022-08-05 12:00:52 +0100

    Update streamlit_app.py
    Exception Corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_List()
   streamlit.dataframe(my_data_rows)
 
-streamlit.dataframe(my_data_row)
-
 #Allow the end user to add a fruit to the list
 add_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('The user entered ', add_fruit)
