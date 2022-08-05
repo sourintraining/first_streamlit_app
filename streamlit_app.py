d1d600ebe78c1c23e55c376e8bdafa62c45684fa
sourintraining <108939079+sourintraining@users.noreply.github.com>
2022-08-05 11:12:21 +0100

    Update streamlit_app.py
    Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
@@ -38,12 +38,6 @@ try:
 
 except URLError as e:
  streamlit.error()
-#import requests
-
-# take the json version of the response and normalise it 
-
-# Output it in the screen as a table
-streamlit.write('The user entered ', fruit_choice)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
